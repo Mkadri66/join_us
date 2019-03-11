bf27417ce4b99d53fac4ffbfd7342b246a7f734a
mkadri66 <you@example.com>
2019-03-11 18:40:07 +0100

    fix(front) : fix front design

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,7 +4,6 @@ namespace AppBundle\Entity;
 
 use PartyBundle\Entity\Party;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
