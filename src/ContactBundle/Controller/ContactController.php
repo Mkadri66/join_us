6dd65bf8b007c6e667e50426823a0fb548ddde5d
Mustapha Kadri <mustapha.kadri66@gmail.com>
2019-03-16 19:00:20 +0100

    fix (contact) : add date message

--- src/ContactBundle/Controller/ContactController.php
+++ src/ContactBundle/Controller/ContactController.php
@@ -2,10 +2,12 @@
 
 namespace ContactBundle\Controller;
 
+use DateTime;
 use ContactBundle\ContactBundle;
-use ContactBundle\Entity\Contact;
 
+use ContactBundle\Entity\Contact;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -36,6 +38,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
         if ($form->isSubmitted() && $form->isValid()) {
             $contactMessage = $form->getData();
+            
+            $contactMessage->setSendAt(new DateTime());
             $em = $this->getDoctrine()->getManager();
             $em->persist($contactMessage);
             $em->flush();
